dcbece9c19233006a3bee74a74c0271bf11afa05
rudolfssau <rudolfssau@gmail.com>
2021-04-04 10:52:51 +0300

    Adds sign in template

--- resources/views/Includes/master.blade.php
+++ resources/views/Includes/master.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
     @yield('content')
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,11 @@
 <section id="form-signin-container">
     <form class="form-signin">
         <h1 class="form-signin-h1">Please Sign In!</h1>
-        <label for="">Email</label>
-        <input type="text">
-        <label for="">Password</label>
-        <input type="text">
-        <label for=""><input type="checkbox">Remember Me</label>
-        <button type="submit"></button>
-        <p>2019-2021</p>
+        <input id="inputEmail" type="text" placeholder="Email Address" class="formControl">
+        <input id="inputPassword" type="text" placeholder="Password" class="formControl">
+        <label for="rememberME" id="forumRememberMe"><input type="checkbox" id="forumRememberMeCheckbox" value="remember-me"> Remember me</label>
+        <button type="submit" id="forumSignInbutton">Sign In</button>
+        <p id="formcopyrightText">© 2017-2021</p>
     </form>
 </section>
 @endsection
